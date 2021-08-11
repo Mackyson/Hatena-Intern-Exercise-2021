e5b518697308d5bd22173f16e77b7dbc3e53815f
Mackyson <mackyson72@gmail.com>
2021-08-12 05:53:32 +0900

    generate infinite FizzBuzz results

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -1,8 +1,27 @@
+import "regenerator-runtime/runtime";
+
 export function initializeFizzBuzz(
   list: HTMLOListElement,
   button: HTMLButtonElement
 ): void {
   console.debug("initializeFizzBuzz", list, button);
+	let tmp = fizzbuzz()
+	button.onclick = function(){console.log(tmp.next())}
+	//on"C"lickと記述して一時間溶かした。
+}
 
-  // Implement Me!
+function* fizzbuzz(): IterableIterator<string>{
+	let n=1;
+	while(true) {
+		if (n%3===0 && n%5===0){
+			yield "FizzBuzz"
+		} else if (n%3===0) {
+			yield "Fizz"
+		} else if (n%5===0) {
+			yield "Buzz"
+		} else {
+			yield n.toString()
+		}
+		n++
+	}
 }
